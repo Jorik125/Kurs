655ca1eaa6dc1be7ff1a6f226536570feaaa18ab
Jorik125 <142978734+Jorik125@users.noreply.github.com>
2023-08-31 01:52:02 +0300

    Установил пакет PHPMailer и проверил работоспособность данной библиотеки

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -13,6 +13,9 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class SiteController extends Controller
 {
@@ -98,6 +101,35 @@ class SiteController extends Controller
             $model->date_buy = date('Y-m-d');
 
             $model->save();
+
+            $mail = new PHPMailer(true);
+
+            try {
+                //Server settings
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'mischatitovmail.ru@gmail.com';                     //SMTP username
+                $mail->Password   = 'opwbeikzfuxugyvb';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                //Recipients
+                $mail->setFrom('ggtitovgg@gmail.com', 'Jhon');
+                $mail->addAddress('ggtitovgg@gmail.com', 'Mishanya');     //Add a recipient
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e){
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
 
             return $this->redirect('/');
         } else{
