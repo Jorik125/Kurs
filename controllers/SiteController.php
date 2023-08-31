d7bf0f92243bf0e8efe020ec6cb1297e3da290b0
Jorik125 <142978734+Jorik125@users.noreply.github.com>
2023-08-31 10:55:09 +0300

    Оформил отправку чека на почту

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -100,36 +100,9 @@ class SiteController extends Controller
             $model->type_tickets_id = $post['TicketsBuy']['type_tickets_id'];
             $model->date_buy = date('Y-m-d');
 
+            $this->sendMailPayment($model, $id);
+
             $model->save();
-
-            $mail = new PHPMailer(true);
-
-            try {
-                //Server settings
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'mischatitovmail.ru@gmail.com';                     //SMTP username
-                $mail->Password   = 'opwbeikzfuxugyvb';                               //SMTP password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                //Recipients
-                $mail->setFrom('ggtitovgg@gmail.com', 'Jhon');
-                $mail->addAddress('ggtitovgg@gmail.com', 'Mishanya');     //Add a recipient
-
-                //Content
-                $mail->isHTML(true);                                  //Set email format to HTML
-                $mail->Subject = 'Here is the subject';
-                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                $mail->send();
-                echo 'Message has been sent';
-            } catch (Exception $e){
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            }
 
             return $this->redirect('/');
         } else{
@@ -138,7 +111,57 @@ class SiteController extends Controller
                 'model'=>$model
             ]);
         }
+    }
 
+    public function sendMailPayment($modelBuyTickets, $idTypeTicket){
+
+        $mail = new PHPMailer(true);
+        $typeTicket = TypeTickets::findOne($idTypeTicket);
+
+            try {
+                //Server settings
+                $mail->SMTPDebug = 0;                      //Enable verbose debug output (SMTP::)
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'mischatitovmail.ru@gmail.com';                     //SMTP username
+                $mail->Password   = 'opwbeikzfuxugyvb';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;
+                $mail->CharSet = 'UTF-8'; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                //Recipients
+                $mail->setFrom($modelBuyTickets->email, 'E Three comp');
+                $mail->addAddress($modelBuyTickets->email, 'Daniil');     //Add a recipient
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Чек о покупки билета на выставку';
+                $mail->Body    = '
+                        <table border="1">
+                            <tr>
+                                <th>Имя покупателя</th>
+                                <th>Привязанная почта для отправки чека на почту</th>
+                                <th>Тип билета</th>
+                                <th>Цена</th>
+                                <th>Дата покупки</th>
+                            </tr>
+                            <tr>
+                                <td>'.$modelBuyTickets->name.'</td>
+                                <td>'.$modelBuyTickets->email.'</td>
+                                <td>'.$typeTicket->name.'</td>
+                                <td>'.$typeTicket->price.'</td>
+                                <td>'.$modelBuyTickets->date_buy.'</td>
+                            </tr>
+                        </table>
+                    ';
+//                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients'; // Это тело в виде обычного текста для почтовых клиентов, не поддерживающих HTML.
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e){
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
     }
 
 }
